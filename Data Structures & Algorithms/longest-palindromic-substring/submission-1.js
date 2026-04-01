982f531d5629450ef99ef464a6101e3c4f2c1a86
Jasper Kajiru <kajirujasper@icloud.com>
2026-04-01 14:07:18 -0700

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+
+        let len = 0;
+        let longestStrRange = [0,0]; 
+
+        for(let i = 0; i < s.length; i++){
+            let l = i;
+            let r = i; 
+
+            while(l >=0 && r < s.length && s[r] === s[l]){
+                if((r - l + 1) > len){
+                    len = r - l + 1;
+                    longestStrRange = [l, r];
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i+1;
+            while(l >=0 && r < s.length && s[r] === s[l]){
+                if((r - l + 1) > len){
+                    len = r - l + 1;
+                    longestStrRange = [l, r];
+                }
+                l--;
+                r++;
+            }
+        }
+        return s.slice(longestStrRange[0], longestStrRange[1] + 1);
+    }
+}
