12467cfcdf933b6082795dffdf129ebd25bd9480
Jasper Kajiru <kajirujasper@icloud.com>
2026-03-31 22:14:21 -0700

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        const key = (i, s1, s2) => `${i}-${s1}-${s2}`;
+        const cache = new Map();
+
+        const dfs = (i, ss1, ss2) => {
+            const s1 = ss1.reduce((sum,index) => sum+index, 0)
+            const s2 = ss2.reduce((sum,index) => sum+index, 0) 
+            const cacheKey = key(i, s1, s2);
+            if(i === nums.length) return s1 === s2;           
+            if(cache.has(cacheKey)) return cache.get(cacheKey);
+        
+            const ans = dfs(i+1, [...ss1, nums[i]], ss2) || dfs(i+1, ss1, [...ss2, nums[i]]);
+            cache.set(cacheKey, ans);
+            return ans;
+        }
+
+        
+
+        return dfs(0, [], [], cache)
+
+
+    }
+}
