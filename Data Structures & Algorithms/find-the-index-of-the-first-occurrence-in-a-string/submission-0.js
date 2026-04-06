f5bab83bde78f26638743a514d99a6f342695a9e
Jasper Kajiru <kajirujasper@icloud.com>
2026-04-05 19:20:59 -0700

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {string} haystack
+     * @param {string} needle
+     * @return {number}
+     */
+    strStr(haystack, needle) {
+        return haystack.indexOf(needle)
+    }
+}
