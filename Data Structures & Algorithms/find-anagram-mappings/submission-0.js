d3ea017f225dfa602bfb25b78973c2ed1383af0c
Jasper Kajiru <kajirujasper@icloud.com>
2026-04-09 23:16:11 -0700

    Add: find-anagram-mappings - submission-0

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    anagramMappings(nums1, nums2) {
+        return nums1.map(num =>nums2.indexOf(num))
+    }
+}
