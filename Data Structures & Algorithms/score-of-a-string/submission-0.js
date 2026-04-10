3c79e32ebdd1d382a57506d80192bdaa4708bea4
Jasper Kajiru <kajirujasper@icloud.com>
2026-04-09 22:50:02 -0700

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s) {
+        let sum = 0;
+        for(let i= 1; i < s.length; i++){
+            // console.log(i, s.charAt(i));
+            sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i-1))
+        }
+        return sum;
+    }
+}
