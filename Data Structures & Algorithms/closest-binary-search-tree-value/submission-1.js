613468e095ac9db6e96fa29bf8f24c4c1b76acbc
Jasper Kajiru <kajirujasper@icloud.com>
2026-04-18 20:08:03 -0700

    Add: closest-binary-search-tree-value - submission-1

---
+++ Data Structures & Algorithms/closest-binary-search-tree-value/submission-1.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} target
+     * @return {number}
+     */
+    closestValue(root, target) {
+        let closest = { value: -1, diff: Infinity }; // value, diff
+        let curr = root;
+        while (curr) {
+            const currDiff = Math.abs(curr.val - target);
+            if (
+                currDiff < closest.diff ||
+                (currDiff === closest.diff && curr.val < closest.value)
+            ) {
+                closest = { value: curr.val, diff: currDiff };
+            }
+            if (curr.val > target) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        return closest.value;
+    }
+}
